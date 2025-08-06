efe0a3a45abe60d641253ad56bc514b86899e2d5
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-08-06 23:01:52 +0900

    생성자

---
+++ src/construct/ex/BookMain.java
@@ -0,0 +1,17 @@
+package construct.ex;
+
+public class BookMain {
+    public static void main(String[] args) {
+        // 기본 생성자 사용
+        Book book1 = new Book();
+        book1.displayInfo();
+
+        // title과 author만을 매개변수로 받는 생성자
+        Book book2 = new Book("Hello Java", "Seo");
+        book2.displayInfo();
+
+        // 모든 필드 매개변수로 받는 생성자
+        Book book3 = new Book("Spring", "park", 700);
+        book3.displayInfo();
+    }
+}
