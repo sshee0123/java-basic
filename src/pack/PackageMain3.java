bc7fd84ba7f7ea5f43c2a8ca833008957313176e
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-08-08 22:12:30 +0900

    패키지

---
+++ src/pack/PackageMain3.java
@@ -0,0 +1,10 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain3 {
+    public static void main(String[] args) {
+        User usera = new User();
+        pack.b.User userb = new pack.b.User();
+    }
+}
